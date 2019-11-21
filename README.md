78054888a8b07db85fb5190869b661a1f4a2f96c
Bersder3000 <B1747322151@outlook.com>
2019-11-21 17:21:47 +0800

    bgmApi added

---
+++ README.md
@@ -0,0 +1,6 @@
+# pyWorkshop
+> 用 python 乱搞的一些东西
+> Mess with python
+
+## introduction
+懒得介绍了 \_(:3」∠)\_, 基本都是些爬虫和小工具, 杂七杂八的东西, 随便看看就行(有注释)
--- apis/bangumiApi.py
+++ apis/bangumiApi.py
@@ -1,16 +1,126 @@
 import requests
 import json
-from paUtil import *
-
-loginUrl = 'http://bangumi.tv/login'
-
-raw = "chii_theme_choose=1; __utmc=1; __utmz=1.1570718890.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; chii_theme=light; chii_cookietime=0; chii_auth=JvbY0qlYR5Woe3ElHFAASDp4Z15qMgpLWBXN3CwesnQ63oS%2F8Za3n2CGSsmvecHk8QGKDOtASm%2BMG46TmwYXmeHZKaT2jK%2B%2FYbtv; __utma=1.1766538390.1570718890.1570718890.1570723181.2; chii_searchDateLine=1570723450; chii_sid=jFJqqU; __utmb=1.28.10.1570723181"
-
-headers = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
-}
+from bs4 import BeautifulSoup
 
 
-r = requests.get('http://bangumi.tv/subject/245665', headers=headers,cookies=cookie2dict(raw))
-r.encoding = 'utf-8'
-print(r.text)
+class Bangumi:
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+
+    def __init__(self, username):
+        self.username = str(username)
+        self.nickname = None
+        self.auth = None
+        self.auth_encode = None
+
+    def login(self, email, pwd):
+        r = requests.post('https://api.bgm.tv/auth?source=BGMYetu', {'username': email, 'password': pwd}, headers=self.headers)
+        data = json.loads(r.text)
+        if 'error' in data.keys():
+            raise Exception('login fail: unmatched account or password')
+        self.username = data['username']
+        self.nickname = data['nickname']
+        self.auth = data['auth']
+        self.auth_encode = data['auth_encode']
+
+    def set_username(self, username):
+        """
+        重新设置用户名
+        :param username:
+        :return:
+        """
+        self.username = str(username)
+        return self
+
+    def get_progress(self):
+        """
+        获取追番进度进度
+        :return:
+        """
+        r = requests.get('https://api.bgm.tv/user/%s/collection?cat=playing' % self.username, headers=self.headers)
+        data = json.loads(r.text)
+        return data if data else []
+
+    def anime_watching(self, order=''):
+        """
+        获取正在观看的番(不含进度)
+        :param order: rate/date/title
+        :return:
+        """
+        r = requests.get('http://bangumi.tv/anime/list/%s/do' % self.username, params={'orderby': order}, headers=self.headers)
+        r.encoding = 'utf-8'
+        return self.__anime_extract(r.text)
+
+    def anime_watched(self, order=''):
+        """
+        获取已经观看的番
+        :param order: 同上
+        :return:
+        """
+        r = requests.get('http://bangumi.tv/anime/list/%s/collect' % self.username, params={'orderby': order}, headers=self.headers)
+        r.encoding = 'utf-8'
+        return self.__anime_extract(r.text)
+
+    def anime_wish(self, order=''):
+        """
+        获取想看的番
+        :param order: 同上
+        :return:
+        """
+        r = requests.get('http://bangumi.tv/anime/list/%s/wish' % self.username, params={'orderby': order}, headers=self.headers)
+        r.encoding = 'utf-8'
+        return self.__anime_extract(r.text)
+
+    @staticmethod
+    def __anime_extract(html):
+        """
+        用于信息提取,私有
+        :param html:
+        :return:
+        """
+        soup = BeautifulSoup(html, 'lxml')
+        animes = []
+        for each in soup.find('ul', id='browserItemList').find_all('li'):
+            anime = dict()
+            anime['id'] = each['id'].split('_')[-1]
+            anime['link'] = 'https://bangumi.tv' + each.a['href']
+            anime['imgSrc'] = 'https:' + each.a.span.img['src'].replace('/s/', '/c/')
+            anime['nameCN'] = each.div.h3.a.string
+            anime['name'] = each.div.h3.small.string
+
+            # tmp = each.div.p.string.split('/')
+            # anime['epNum'] = int(tmp[0].strip()[:-1])
+            # anime['date'] = tmp[1].strip()
+            # anime['director'] = tmp[2].strip()
+            # anime['ow'] = tmp[3].strip()  # 原作设定
+            # anime['cs'] = tmp[4].strip()  # 人物设定
+
+            sc_node = each.div.find('span', class_='starstop-s')
+            anime['sc'] = int(sc_node.span['class'][-1][5:]) if sc_node else None  # 个人评分
+            anime['at'] = each.div.find('span', class_='tip_j').string  # 加入时间
+
+            comment_node = each.div.find('div', class_='text_main_even')  # 吐嘈评价
+            anime['comment'] = comment_node.div.string if comment_node else ''
+
+            animes.append(anime)
+        return animes
+
+    @classmethod
+    def get_anime_detail(cls, id_, more=False):
+        """
+        获取某个番的信息
+        :param id_: 番id
+        :param more: 是否详细
+        :return:
+        """
+        r = requests.get('https://api.bgm.tv/subject/%s' % id_, {'responseGroup': 'large' if more else 'simple'}, headers=cls.headers)
+        return json.loads(r.text)
+
+
+# b = Bangumi(504947)
+# print(b.get_progress())
+# print(b.anime_wish())
+# print(b.anime_watched())
+# print(b.anime_watching())
+# print(b.get_anime_detail(185792))
